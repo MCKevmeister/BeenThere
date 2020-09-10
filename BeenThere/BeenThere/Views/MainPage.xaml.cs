5983cba755d9ba999bb0a3c3de572244de3f7bb0
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2020-09-10 14:08:29 +1200

    creating app

---
+++ BeenThere/BeenThere/Views/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using BeenThere.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BeenThere
+{
+    public partial class Main : ContentPage
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private async void onSearchClicked(object sender, EventArgs e)
+        {
+            var searchPage = new Search();
+            await Navigation.PushModalAsync(searchPage);
+        }
+
+        private async void onAccountClicked(object sender, EventArgs e)
+        {
+            var accountPage = new Account();
+            await Navigation.PushModalAsync(accountPage);
+        }
+
+    }
+}
